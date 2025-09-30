ff8bf52abe5529fa64e59b518321c91f769fb32c
sachin <sachinshrestha321@gmail.com>
2025-09-30 21:49:51 +1000

    Define AppController class

--- controllers/app_controller.py
+++ controllers/app_controller.py
@@ -5,3 +5,6 @@ from utils.decorators import log_action, timing_decorator, error_handler
 from models.text_sentiment import TextSentimentAdapter
 from models.image_classifier import ImageClassifierAdapter
 from models.text_to_image_fast import TextToImageAdapterFast as TextToImageAdapter
+
+class AppController(LoggingMixin, ConfigurableMixin):
+    """Main application controller - demonstrates multiple inheritance"""
