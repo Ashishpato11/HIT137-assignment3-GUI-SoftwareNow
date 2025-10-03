09b8abcf045404d79930bd5ea4e44340a7a9faf7
JUNG <jung@BishalJungChhetri.local>
2025-10-03 17:05:01 +1000

    defined timing_decorator to wrap the execution in timer and log the elapsed time

--- utils/decorators.py
+++ utils/decorators.py
@@ -13,3 +13,21 @@ def log_action(logger_attr: str = '_logger'):
             return result
         return wrapper
     return decorator
+
+def timing_decorator(func):
+    """Decorator to measure execution time"""
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        start_time = time.time()
+        result = func(self, *args, **kwargs)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        
+        # Log timing if logger exists
+        if hasattr(self, '_logger'):
+            self._logger(f'[TIMING] {self.__class__.__name__}.{func.__name__} took {execution_time:.4f}s')
+        else:
+            print(f'[TIMING] {self.__class__.__name__}.{func.__name__} took {execution_time:.4f}s')
+        
+        return result
+    return wrapper
