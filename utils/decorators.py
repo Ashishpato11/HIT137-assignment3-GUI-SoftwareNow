4468efd1c7d6dd3b108121e049787de02b9e13ca
JUNG <jung@BishalJungChhetri.local>
2025-10-03 17:02:43 +1000

    defined log_action decorator to log the entry and exit of the decorated method

--- utils/decorators.py
+++ utils/decorators.py
@@ -1,2 +1,15 @@
 from functools import wraps
 import time
+
+def log_action(logger_attr: str = '_logger'):
+    """Decorator to log method entry and exit"""
+    def decorator(func):
+        @wraps(func)
+        def wrapper(self, *a, **k):
+            logger = getattr(self, logger_attr, None)
+            if logger: logger(f'[ENTER] {self.__class__.__name__}.{func.__name__}')
+            result = func(self,*a,**k)
+            if logger: logger(f'[EXIT ] {self.__class__.__name__}.{func.__name__}')
+            return result
+        return wrapper
+    return decorator
